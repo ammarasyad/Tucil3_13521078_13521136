3be5783a5ab1c7735379b77262d4447028f45989
ammarasyad <ammarasyad@gmail.com>
2023-04-10 23:55:46 +0700

    feat: Remade A* algorithm from the ground up to support adjacency matrix input

--- src/algorithm/a_star.py
+++ src/algorithm/a_star.py
@@ -1,156 +1,71 @@
-import math
-from typing import List
+from math import sqrt
 
-def convert(adj: dict):
-    return {k: {(v, w) for v, w in adj[k]} for k in adj}
+class Node:
+    def __init__(self, label, weight=0, x=0, y=0):
+        self.label = str(label)
+        self.weight = weight
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.label == other.label
+
+    def __repr__(self):
+        return f'{self.label}'
+
+    def __hash__(self):
+        return hash(self.label)
 
 
-def get_path(came_from, current):
+def build(matrix):
+    return {Node(i): [Node(j, matrix[i][j], i, j) for j in range(len(matrix[i])) if matrix[i][j] > 0] for i in range(len(matrix))}
+
+def retrace_path(came_from, current):
     path = [current]
     while current in came_from:
         current = came_from[current]
         path.append(current)
     return path[::-1]
 
-
-class Node:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __getitem__(self, item):
-        return (self.x, self.y)[item]
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-    def __repr__(self):
-        return f'({self.x}, {self.y})'
-
-
-def get_neighbors(matrix, i, j):
-    neighbors = set()
-    if i > 0 and matrix[i - 1][j] == 1:
-        neighbors.add(Node(i - 1, j))
-    if i < len(matrix) - 1 and matrix[i + 1][j] == 1:
-        neighbors.add(Node(i + 1, j))
-    if j > 0 and matrix[i][j - 1] == 1:
-        neighbors.add(Node(i, j - 1))
-    if j < len(matrix[i]) - 1 and matrix[i][j + 1] == 1:
-        neighbors.add(Node(i, j + 1))
-    return neighbors if len(neighbors) > 0 else None
-
-
-class Map:
-    def __init__(self, adj_matrix: List[List[int]]):
-        self.adj_matrix = self.build_map(adj_matrix)
-
-    def build_map(self, matrix):
-        return {Node(i, j): get_neighbors(matrix, i, j) for i in range(len(matrix)) for j in range(len(matrix[i]))}
-
-    def __getitem__(self, item):
-        return self.adj_matrix[item]
-
-    def __contains__(self, item):
-        return item in self.adj_matrix.keys()
-
-
 class Graph:
-    def __init__(self, adj_list, start, end):
-        if not isinstance(adj_list, Map):
-            adj_list = Map(adj_list)
-        # if not isinstance(adj_list, dict):
-        #     adj_list = convert(adj_list)
-        self.adj_list = adj_list
-        if not isinstance(start, Node):
-            start = Node(*start)
-        if not isinstance(end, Node):
-            end = Node(*end)
-
-        self.start = start
-        self.end = end
+    def __init__(self, matrix, start, goal):
+        self.nodes = build(matrix)
+        self.start = Node(start)  # Start Node
+        self.goal = Node(goal)  # Goal Node
 
         # Input validation
-        if self.start not in self.adj_list:
-            raise ValueError(f'{self.start} is not in the graph')
-        if self.end not in self.adj_list:
-            raise ValueError(f'{self.end} is not in the graph')
+        if self.start not in self.nodes:
+            raise Exception(f'Start node {self.start} not in graph')
+        if self.goal not in self.nodes:
+            raise Exception(f'Goal node {self.goal} not in graph')
 
-    def set_start(self, start):
-        self.start = start
-
-    def set_end(self, end):
-        self.end = end
-
-    def get_neighbors(self, v):
-        return self.adj_list[v]
-
-    def get_heuristic(self, v):
-        return math.sqrt((self.end[0] - v[0]) ** 2 + (self.end[1] - v[1]) ** 2)
+    def euclidean_distance(self, node):
+        return sqrt((node.x - self.goal.x) ** 2 + (node.y - self.goal.y) ** 2)
 
     def calculate(self):
-        open_set = {self.start}  # set of nodes to be evaluated
-        closed_set = set()  # set of nodes already evaluated
-        optimal_path = {self.start: 0}  # distance from start along optimal path
-        nav_map = {self.start: self.start}  # map of navigated nodes
+        open_set = {self.start}
+        came_from = {}
+        g_score = {self.start: 0}
+        f_score = {self.start: self.euclidean_distance(self.start)}
 
         while len(open_set) > 0:
-            current = None
-            for v in open_set:
-                if current is None or nav_map[v] + self.get_heuristic(v) < nav_map[current] + self.get_heuristic(current):
-                    current = v
-
-            if current is None:
-                break
-
-            if current == self.end:
-                path = []
-
-                while nav_map[current] != current:
-                    path.append(current)
-                    current = nav_map[current]
-
-                path.append(current)
-                return path[::-1]
-
-            # for node, weight in self.get_neighbors(current):
-            #     if node not in open_set and node not in closed_set:
-            #         open_set.add(node)
-            #         nav_map[node] = current
-            #         optimal_path[node] = optimal_path[current] + weight
-            #     else:
-            #         if optimal_path[current] + weight < optimal_path[node]:
-            #             optimal_path[node] = optimal_path[current] + weight
-            #             nav_map[node] = current
-            #
-            #             if node in closed_set:
-            #                 closed_set.remove(node)
-            #                 open_set.add(node)
-
+            current = min(open_set, key=lambda x: f_score[x])
+            if current == self.goal:
+                return retrace_path(came_from, current)
             open_set.remove(current)
-            closed_set.add(current)
-
+            for neighbor in self.nodes[current]:
+                tentative_g_score = g_score[current] + neighbor.weight
+                if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                    came_from[neighbor] = current
+                    g_score[neighbor] = tentative_g_score
+                    f_score[neighbor] = tentative_g_score + self.euclidean_distance(neighbor)
+                    if neighbor not in open_set:
+                        open_set.add(neighbor)
         return None
 
-
 if __name__ == '__main__':
-    adj_matrix = [[0, 1, 0, 0, 0, 0, 0, 0],
-                  [1, 0, 1, 1, 1, 0, 0, 0],
-                  [0, 1, 0, 0, 0, 1, 0, 0],
-                  [0, 1, 0, 0, 0, 1, 0, 0],
-                  [0, 1, 0, 0, 0, 1, 0, 0],
-                  [0, 0, 1, 1, 1, 0, 1, 1],
-                  [0, 0, 0, 0, 0, 1, 0, 0],
-                  [0, 0, 0, 0, 0, 1, 0, 0]]
-    # matr = {
-    #     (0, 0): {((1, 0), 1), ((0, 1), 1)},
-    #     (0, 1): {((1, 1), 1), ((0, 0), 1)},
-    #     (1, 0): {((0, 0), 1), ((1, 1), 1)},
-    #     (1, 1): {((0, 1), 1), ((1, 0), 1)},
-    # }
-    graph = Graph(adj_matrix, (0, 0), (7, 7))
+    with open('test.txt') as f:
+        adj_matrix = [[int(x) for x in line.split()] for line in f.readlines()]
+    graph = Graph(adj_matrix, 0, 7)
 
     print(graph.calculate())
