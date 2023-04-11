5180dc78498a6c41f60be34e9bd52395aa9d5c78
ammarasyad <ammarasyad@gmail.com>
2023-04-11 19:54:53 +0700

    chore: UCS code cleanup to match PEP and simplify code

--- src/algorithm/ucs.py
+++ src/algorithm/ucs.py
@@ -1,83 +1,70 @@
-  
+def check_node_in_route(route, node):
+    return node in route
+
+
 class UCS:
     def __init__(self, matrix):
         self.matrix = matrix
         self.queue = []
         
-    def getWeight(self, node1, node2):
+    def get_weight(self, node1, node2):
         return self.matrix[node1-1][node2-1]
     
-    def totalWeight(self, route):
-        total = 0
+    def total_weight(self, route):
+        return sum([self.get_weight(route[i], route[i + 1]) for i in range(len(route)-1)])
+
+    def push_route(self, route):
         i = 0
-        while (i < len(route)-1):
-            total += self.getWeight(route[i], route[i+1])
-            i += 1
-            
-        return total
-    
-    def pushRoute(self, route):
-        i = 0
-        if (len(self.queue) > 0):
-            while (len(self.queue) > i):
-                if (self.totalWeight(self.queue[i]) <= self.totalWeight(route)):
-                    i += 1
-                else:
+        if len(self.queue) > 0:
+            while len(self.queue) > i:
+                if self.total_weight(self.queue[i]) > self.total_weight(route):
                     break
-                
-        
+                i += 1
+
         self.queue.insert(i, route)
         return
-    
-    def checkNodeInRoute(self, route, node):
-        for n in route:
-            if n == node:
-                return True
-                
-        return False
-    
-    def getNextNode(self, routeNow):
-        lastNode = routeNow[-1]
-        nextNode = []
-        edges = self.matrix[lastNode-1]
+
+    def get_next_node(self, current_route):
+        last_node = current_route[-1]
+        next_node = []
+        edges = self.matrix[last_node-1]
         
-        i = 0
         for i in range(len(edges)):
-            if (edges[i] != 0 and not self.checkNodeInRoute(routeNow, i+1)):
-                nextNode.append(i+1)
+            if edges[i] != 0 and not check_node_in_route(current_route, i + 1):
+                next_node.append(i+1)
         
-        return nextNode
+        return next_node
     
     def search(self, source, destination):
         self.queue.append([source])
         solution = []
-        while(len(self.queue) > 0):
-            routeNow = self.queue.pop(0)
+        while len(self.queue) > 0:
+            route_now = self.queue.pop(0)
             
-            if (routeNow[0] == source and routeNow[-1] == destination):
-                solution = routeNow
+            if route_now[0] == source and route_now[-1] == destination:
+                solution = route_now
                 break
             
-            nextNode = self.getNextNode(routeNow)
-            for node in nextNode:
-                temp = routeNow.copy()
+            next_node = self.get_next_node(route_now)
+            for node in next_node:
+                temp = route_now.copy()
                 temp.append(node)
-                self.pushRoute(temp)
+                self.push_route(temp)
             
             
-        return solution, self.totalWeight(solution)
+        return solution, self.total_weight(solution)
 
 
 if __name__ == '__main__':
     matrix = []
-    while (not matrix):
+    while not matrix:
         try:
             filename = input("Masukkan nama file: ")
             with open(filename, 'r') as f:
                 for line in f:
                     temp = [int(num) for num in line.split(' ')]
                     matrix.append(temp)
-        except:
+        except IOError | ValueError | FileNotFoundError:
             print("Masukkan file lain")
             
     ucs = UCS(matrix)
